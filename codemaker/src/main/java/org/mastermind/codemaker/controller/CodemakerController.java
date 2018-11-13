ba39967ed4d44a1fedf0d6a5706ee7e8f5a51061
Marcelo Aldana <aldana.marcelo@gmail.com>
2018-11-13 13:24:32 +0100

    make guesses

--- codemaker/src/main/java/org/mastermind/codemaker/controller/CodemakerController.java
+++ codemaker/src/main/java/org/mastermind/codemaker/controller/CodemakerController.java
@@ -1,10 +1,14 @@
 package org.mastermind.codemaker.controller;
 
 import org.mastermind.codemaker.dto.GameDto;
+import org.mastermind.codemaker.dto.GuessResultDto;
+import org.mastermind.codemaker.model.CodePegEnum;
 import org.mastermind.codemaker.service.CodemakerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +26,16 @@ public class CodemakerController {
 	@GetMapping(path="/new")
 	public @ResponseBody GameDto createNewGame() {
 		return service.newGame();
+	}
+
+	@GetMapping(path="/{id}/guess")
+	public @ResponseBody GuessResultDto makeGuess(@PathVariable Integer id,
+										   		  @RequestParam CodePegEnum color1,
+										   		  @RequestParam CodePegEnum color2,
+										   		  @RequestParam CodePegEnum color3,
+										   		  @RequestParam CodePegEnum color4) {
+		
+		return service.guess(id, color1, color2, color3, color4);
 	}
 
 }
